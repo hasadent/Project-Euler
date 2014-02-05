790fb9f4e95481b1fc44f56b419c1dc10f05e57f
hasadent <hasadent@users.noreply.github.com>
2014-02-05 19:54:11 +0800

    Create 5.c

---
+++ 5.c
@@ -0,0 +1,33 @@
+/****
+ * Problem 5
+ */
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+int main()
+{
+    int i, j, k;
+    int max = 0;
+
+    int value[19], *p = value;
+    
+    /* set value from 2 to 19 */
+    for (i = 2; i <= 20;  i++ )
+        *p++ = i;
+
+    /* remove common divisor */
+    for (i = 1; i < 19; i++)
+        for (j = 0; j < i; j++)
+            if (value[j] != 1 && value[i] % value[j] == 0)
+                value[i] /= value[j];
+
+    /* get answer */
+    k = 1;
+    for (i = 0; i < 19; i++)
+        k *= value[i];
+
+    printf("%d\n", k);
+
+    return 0;
+}
