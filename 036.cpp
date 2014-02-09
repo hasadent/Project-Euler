a0ff651a6f821f32ee540ecfd45dd30abfe88bad
hasadent <hasadent@users.noreply.github.com>
2014-02-09 12:48:58 +0800

    Update 036.cpp

--- 036.cpp
+++ 036.cpp
@@ -28,7 +28,7 @@ int main()
 {
     int sum = 0;
     for (int i = 1; i <= 1000000; i+= 2)
-        if (i % 10 != 0 && is_palindrome(i, 10) && is_palindrome(i,2 ))
+        if (i % 10 != 0 && is_palindrome(i, 10) && is_palindrome(i, 2))
             sum += i;
     cout << "The sum is " << sum << "." << endl;
 
